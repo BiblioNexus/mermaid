1a31dbb83f374c7de43dc9f69d982700c3a4ae54
hiroshi <hiroshi.tan57@gmail.com>
2024-02-27 12:22:32 -0300

    feat: add svg drawers

---
+++ packages/mermaid/src/diagrams/simpleGrammar/svgDrawer/drawAdjectivalClauseDecorator.ts
@@ -0,0 +1,61 @@
+import * as d3 from 'd3';
+
+import { settings } from '../settings.js';
+
+import type { DrawUnit } from '../simpleGrammarTypes.js';
+
+export function drawAdjectivalClauseDecorator(): DrawUnit {
+  const d3Elem = d3.create('svg:g');
+
+  const triangleEdge = 2 * settings.padding;
+  const triangleHeight = (triangleEdge * Math.sqrt(3)) / 2;
+
+  const width = settings.padding + 2 * settings.height;
+  const height = 2 * settings.height + triangleHeight;
+
+  const startX = 0;
+
+  const data: [number, number][][] = [
+    [
+      [startX, settings.height],
+      [startX, 2 * settings.height],
+      [startX - triangleEdge / 2, height],
+      [startX + triangleEdge / 2, height],
+      [startX, 2 * settings.height],
+    ],
+    [
+      [startX - triangleEdge / 2 - settings.padding, height],
+      [startX + triangleEdge / 2, height],
+      [startX + 2 * settings.height, 0],
+      [startX + settings.padding, 0],
+    ],
+  ];
+
+  const lineGenerator = d3
+    .line()
+    .x((d) => d[0])
+    .y((d) => d[1]);
+
+  for (let i = 0; i < data.length; i++) {
+    const lineData = data[i];
+
+    d3Elem
+      .append('path')
+      .attr('d', lineGenerator(lineData))
+      .attr('fill', 'none')
+      .attr('stroke', settings.wordColor)
+      .attr('stroke-width', settings.lineStrokeWidth);
+  }
+
+  return {
+    width,
+    height,
+    element: d3Elem,
+    verticalStart: 0,
+    verticalCenter: settings.height,
+    verticalEnd: height,
+    herizontalStart: 0,
+    herizontalCenter: 2 * settings.padding,
+    herizontalEnd: width,
+  };
+}
